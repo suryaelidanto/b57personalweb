8d8ab1bea112400f3c26b392c2e6be779d12d975
suryaelidanto <suryaelidanto@gmail.com>
2024-08-30 10:54:22 +0700

    feat: count post blog time

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,3 +1,62 @@
+// count post blog time
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+function getDistanceTime(timePost) {
+  const timeNow = new Date();
+  const distance = timeNow - timePost; // hasilnya miliseconds => 1 detik = 1000ms
+
+  // seconds, minutes, hours, day
+  // round => membulatkan ke angkat terdekat | 7.3 => 7 | 7.5 => 8
+  // floor => membulatkan ke bawah | 7.9 => 7 | 7.99 => 7
+  // ceil => membulatkan ke atas | 8.01 => 9 | 8.3 => 9
+  const seconds = Math.floor(distance / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const day = Math.floor(hours / 24);
+
+  if(seconds < 60) {
+    return `${seconds} seconds ago`
+  } else if (minutes < 60) {
+    return `${minutes} minutes ago`
+  } else if(hours < 60) {
+    return `${hours} hours ago`
+  } else if(day < 24) {
+    return `${day} day ago`
+  }
+}
+
+function getFullTime(time) {
+  const date = time.getDate();
+  const monthIndex = time.getMonth();
+  const year = time.getFullYear();
+  let hours = time.getHours();
+  let minutes = time.getMinutes();
+
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  return `${date} ${months[monthIndex]} ${year} ${hours}:${minutes} WIB`;
+}
+
+// blogs
 const blogs = [];
 function addBlog(event) {
   event.preventDefault();
@@ -39,10 +98,13 @@ function renderBlog() {
                         <a href="blog-detail.html" target="_blank">${blogs[index].title}</a>
                     </h1>
                     <div class="detail-blog-content">
-                        ${blogs[index].createdAt} | ${blogs[index].author}
+                        ${getFullTime(blogs[index].createdAt)} | ${blogs[index].author}
                     </div>
                     <p>
                         ${blogs[index].content}
+                    </p>
+                    <p>
+                      ${getDistanceTime(blogs[index].createdAt)}
                     </p>
                 </div>
             </div>
@@ -52,4 +114,7 @@ function renderBlog() {
   document.getElementById("contents").innerHTML = html;
 }
 
-renderBlog();
+
+setInterval(() => {
+  renderBlog();
+}, 1000)
