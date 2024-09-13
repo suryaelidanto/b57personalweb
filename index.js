797ace7b3f815136b2a14ca27e930e825cf80bf0
suryaelidanto <suryaelidanto@gmail.com>
2024-09-13 11:38:31 +0700

    feat: auth and session

--- index.js
+++ index.js
@@ -2,19 +2,30 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const path = require("path");
-const config = require("./config/config");
-const { Sequelize, QueryTypes, where } = require("sequelize");
-
-const sequelize = new Sequelize(config.development);
+const bcrypt = require("bcrypt");
+const session = require("express-session");
+const flash = require("express-flash");
 
 const blogModel = require("./models").blog;
+const userModel = require("./models").user;
 
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "./views"));
 
 app.use("/assets", express.static(path.join(__dirname, "./assets")));
-
 app.use(express.urlencoded({ extended: false }));
+app.use(
+  session({
+    name: "my-session",
+    secret: "ewVsqWOyeb",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+  })
+);
+app.use(flash());
 
 app.get("/", home);
 app.get("/blog", blog);
@@ -27,14 +38,87 @@ app.get("/contact", contact);
 app.get("/testimonial", testimonial);
 app.get("/blog-detail/:id", blogDetail);
 
+app.get("/login", loginView);
+app.get("/register", registerView);
+
+app.post("/register", register);
+app.post("/login", login);
+
+function loginView(req, res) {
+  res.render("login");
+}
+
+async function login(req, res) {
+  try {
+    const { email, password } = req.body;
+
+    // cek email user apakah ada di database
+    const user = await userModel.findOne({
+      where: {
+        email: email,
+      },
+    });
+
+    if (!user) {
+      req.flash("error", "Email / password salah!");
+      return res.redirect("/login");
+    }
+
+    // cek password apakah valid dengan password yang sudah di hash
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if (!isValidPassword) {
+      req.flash("error", "Email / password salah!");
+      return res.redirect("/login");
+    }
+
+    req.session.user = user;
+
+    req.flash("success", "Login berhasil!");
+
+    res.redirect("/");
+  } catch (error) {
+    req.flash("error", "Something went wrong!");
+    res.redirect("/");
+  }
+}
+
+function registerView(req, res) {
+  res.render("register");
+}
+
+async function register(req, res) {
+  try {
+    const { name, email, password } = req.body;
+
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    await userModel.create({
+      name: name,
+      email: email,
+      password: hashedPassword,
+    });
+
+    req.flash("success", "Register berhasil!");
+    res.redirect("/register");
+  } catch (error) {
+    req.flash("error", "Register gagal!");
+    res.redirect("/register");
+  }
+}
+
 function home(req, res) {
-  res.render("index");
+  const user = req.session.user;
+
+  res.render("index", { user });
 }
 
 async function blog(req, res) {
   const result = await blogModel.findAll();
+  const user = req.session.user;
 
-  res.render("blog", { data: result });
+  res.render("blog", { data: result, user });
 }
 
 async function deleteBlog(req, res) {
